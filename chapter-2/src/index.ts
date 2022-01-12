72f99c8d75d9d136c334c306faf7d7a00364bb31
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2022-01-13 00:54:51 +0900

    [completed]3.2.12

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -293,3 +293,33 @@
 // type E = Readonly<[number, string]>  // readonly [number, string]
 
 
+// 3.2.12 null, undefined, void, never
+// numberまたはnullを返す関数
+// function a(x: number) {
+//     if(x < 10) {
+//         return x
+//     }
+//     return null
+// }
+//
+// // undefinedを返す関数
+// function b() {
+//     return undefined
+// }
+//
+// // voidを返す関数
+// function c() {
+//     let a = 2 + 2
+//     let b = a * a
+// }
+//
+// // neverを返す関数
+// function d() {
+//     throw TypeError('I always Error')
+// }
+// function e() {
+//     white(true) {
+//         doSomething()
+//     }
+// }
+
